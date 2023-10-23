3b89e31e718f5f01484c59a17ee68fe82c6869b1
Samuel Austin Manopo <119147570+Samanopo@users.noreply.github.com>
2023-10-23 15:11:36 +0800

    Add files via upload

---
+++ lecture18/components/Header.js
@@ -0,0 +1,11 @@
+class HeaderPage extends React.Component {
+    render() {
+        return(
+        <div class="header">
+            <h1>Chatbot</h1>
+            <p>Chatbot or chatterbot is a software application used to conduct an online chat conversation via text or text to speech, in lieu of providing direct contact with a live human agent. Chatbot is a computer program that simulates and processes humans to interact with digital devices as if they were communicating with a real person.</p>
+
+        </div>
+        );
+    }
+}
---
+++ lecture18/components/Sidebar.js
@@ -0,0 +1,12 @@
+class SidebarPage extends React.Component {
+    render() {
+        return(
+            <div class="side">
+                <h2>About Me</h2>
+                <h5>My Photo</h5>
+                <div class="fakeimg" style={{heigh: "150px;"}}>Image</div>
+                <p>Hi, My name is S. I really love programming. I hope we can work together</p>
+            </div>
+        );
+    }
+}
