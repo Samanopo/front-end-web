706f9e067b00b86fa7a9de9fdf561af296322d0d
Samuel Austin Manopo <119147570+Samanopo@users.noreply.github.com>
2023-10-29 14:38:58 +0800

    Add files via upload

---
+++ lecture20/components/About.js
@@ -0,0 +1,17 @@
+function About() {
+    return (
+        <div>
+            <h2>
+                Front-end web development is the debelopment of the grapical
+                user interface og  website, thtough the use of HTML, CSS, and
+                JavaScript, so that users cn view and interact with that website.
+            </h2>
+
+            Read more the article :
+            <a href=
+            "https://frontendmasters.com/guides/front-end-handbook/2019/">
+                https://frontendmasters.com/guides/front-end-handbook/2019/"
+            </a>
+        </div>
+    );
+}
---
+++ lecture20/components/Contact.js
@@ -0,0 +1,10 @@
+function Contact() {
+    return(
+        <address>
+            You can find me here:<br />
+            Universitas Klabat<br />
+            Jl. Arnold Mononutu, Armadidi Bawah, Kec. Airmadidi, <br />
+            Kabupaten Minahasa Utara, Sulawesi Utara 95371
+        </address>
+    );
+}
